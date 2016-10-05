fc62eaed9a5bad9069ccfeeac3a540a37d3f4330
Julien Tessier <julien@cahri.com>
2016-10-05 12:44:55 +0400

    first release

---
+++ lang/paquet-logo_svg_fr.php
@@ -0,0 +1,11 @@
+<?php
+
+	if (!defined('_ECRIRE_INC_VERSION')) {
+		return;
+	}
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'logo_svg_nom' => 'Logo SVG',
+		'logo_svg_slogan' => 'Autorise l’utilisation de logos SVG sur les objets éditoriaux',
+	);
+
+?>
---
+++ logo_svg_fonctions.php
@@ -0,0 +1,15 @@
+<?php
+
+	$GLOBALS['formats_logos'] = array('gif', 'jpg', 'png', 'svg');
+
+	function logo_svg_largeur($fichier) {
+		$metadata_svg = charger_fonction('svg', 'metadata');
+		$size = $metadata_svg($fichier);
+		return $size['largeur'];
+	}
+
+	function logo_svg_hauteur($fichier) {
+		$metadata_svg = charger_fonction('svg', 'metadata');
+		$size = $metadata_svg($fichier);
+		return $size['hauteur'];
+	}
