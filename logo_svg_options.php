9d0f10dbc4d8ac0d3592a61cbb09c217fdfb93bb
kent1 <kent1@arscenic.info>
2017-10-31 10:12:53 +0100

    Sécurité

--- logo_svg_options.php
+++ logo_svg_options.php
@@ -1,3 +1,7 @@
 <?php
 
-	$GLOBALS['formats_logos'] = array('gif', 'jpg', 'png', 'svg');
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS['formats_logos'] = array('gif', 'jpg', 'png', 'svg');
